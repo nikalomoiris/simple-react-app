1afd80df012c159963f5688b158945319f6deefc
nikalomiris <nikalomiris@gmail.com>
2019-06-21 11:06:59 -0400

    Configured Firebase database.

---
+++ src/fire.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var config = { /* COPY THE ACTUAL CONFIG FROM FIREBASE CONSOLE */
+    apiKey: "AIzaSyDNFzomSnKq8yfM0QU13p-SpgdpNyUXqVI",
+    authDomain: "simple-react-app-520c2.firebaseapp.com",
+    databaseURL: "https://simple-react-app-520c2.firebaseio.com",
+    projectId: "simple-react-app-520c2",
+    storageBucket: "",
+    messagingSenderId: "165381353945",
+    appId: "1:165381353945:web:d12a33580a894c57"
+};
+var fire = firebase.initializeApp(config);
+export default fire;
